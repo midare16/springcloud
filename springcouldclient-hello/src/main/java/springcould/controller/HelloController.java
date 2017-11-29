fdbf271a548168a947fc2534a4ab396f5835a2a8
yayoi <17001139@qq.com>
2017-11-29 16:17:28 +0800

    up

---
+++ springcouldclient-hello/src/main/java/springcould/controller/HelloController.java
@@ -0,0 +1,17 @@
+package springcould.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/access")
+    @ResponseBody
+    public String access() {
+        return "access success";
+    }
+}
