57cf9263c8f21d8bf482b4e68f5caecef9fc8f06
yayoi <17001139@qq.com>
2017-11-29 14:12:37 +0800

    import

---
+++ springcouldservice/src/main/java/springcould/controller/HelloController.java
@@ -0,0 +1,24 @@
+package springcould.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import springcould.service.HelloService;
+
+@RestController
+public class HelloController {
+	
+	@Resource
+	private HelloService helloService;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return helloService.get();
+    }
+
+}
