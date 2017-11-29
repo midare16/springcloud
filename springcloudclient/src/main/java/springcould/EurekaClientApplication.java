0184f26893c7886797cec68f838783085fb00513
yayoi <17001139@qq.com>
2017-11-29 17:38:36 +0800

    add fallback

--- springcloudclient/src/main/java/springcould/EurekaClientApplication.java
+++ springcloudclient/src/main/java/springcould/EurekaClientApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
